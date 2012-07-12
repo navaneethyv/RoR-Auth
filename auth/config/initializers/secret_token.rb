2ad420a8b1b9b8f9c5435a8131dcbc19e76ac5c4
NavaneethBhat <navneeth.neo@gmail.com>
2012-07-12 15:56:07 +0530

    "Basic_Layout"

---
+++ auth/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auth::Application.config.secret_token = '7365a68e637f1bbf7c9c2a8b93abc869687028c7cd8939e996176d223d67d9fe940f3d5297c2e6f380b010a37a84610b34d156d6a322f6bf2530a815b276267f'
